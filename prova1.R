197e0fe9c9b3c43dcfe38ba412ac1f25cf50d201
Frezza99 <alessiofrezza99@gmail.com>
2022-11-30 23:18:02 +0100

    prova codice banale (GAM)

--- prova1.R
+++ prova1.R
@@ -1,4 +1,4 @@
-setwd("C:/Users/UTENTE/Downloads/Nonparametric Statistics/Project/Electric_Vehicles")
+# setwd("C:/Users/UTENTE/Downloads/Nonparametric Statistics/Project/Electric_Vehicles")
 data = evdatawithprices
 library(ISLR2)
 library(car)
@@ -6,6 +6,7 @@ library(mgcv)
 library(rgl)
 library(splines)
 library(pbapply)
+library(mgcv)
 
 attach(data)
 
@@ -52,19 +53,56 @@ summary(model_lm4)
 plot(model_lm4)
 shapiro.test(residuals(model_lm4))
 
-# proviamo interactions
+#GLM
+# (1)cubic spline basis
+model_gam1=gam(Price ~ s(Electric.Range,bs='cr') 
+                    + s(Vehicle.Fuel.Euivalent,bs='cr')
+                    + s(Charge.Speed,bs='cr') 
+                    + s(Total.Power,bs='cr') 
+                    + s(Height,bs='cr') 
+                    + s(Length,bs='cr') 
+                    + s(Max..Payload,bs='cr') 
+                    + s(Cargo.Volume,bs='cr'), data=data
+              )
+summary(model_gam1)
+# alto R-sq 0.971, ma max.payload sembra poco significativo
 
-model_lm5=lm(Price ~ (Vehicle.Fuel.Euivalent 
-             + Total.Power
-             + Height
-             + Length
-             + Cargo.Volume)^2, data=data)
-summary(model_lm5)
+# tolgo max.paypload
+model_gam2=gam(Price ~ s(Electric.Range,bs='cr') 
+               + s(Vehicle.Fuel.Euivalent,bs='cr')
+               + s(Charge.Speed,bs='cr') 
+               + s(Total.Power,bs='cr') 
+               + s(Height,bs='cr') 
+               + s(Length,bs='cr') 
+               + s(Cargo.Volume,bs='cr'), data=data
+              )
+summary(model_gam2)
+# quasi identico R-sq 0.969, tutte variabili significative
 
-# tolgo height:length
+# residuals
+hist(model_gam2$residuals)
+qqnorm(model_gam2$residuals)
 
+# normality test
+shapiro.test(model_gam2$residuals)
 
-git config --global user.email "alessiofrezza99@gmail.com"
-git config --global user.name "Frezza99"
+# (2)natural cubic splines
+model_gam_ns =
+  lm(Price ~ ns(Electric.Range,df=8) 
+            + ns(Vehicle.Fuel.Euivalent,df=8)
+            + ns(Charge.Speed,df=8) 
+            + ns(Total.Power,df=8) 
+            + ns(Height,df=8) 
+            + ns(Length,df=8) 
+            + ns(Cargo.Volume,df=8), data=data
+     )
+# summary(model_gam_ns)
 
-Omit --global 
+# scatterplot for the two models
+plot(model_gam_ns$residuals,model_gam2$residuals)
+cor(model_gam_ns$residuals,model_gam2$residuals)
+
+# Consider model with cubic spline basis
+# I look at the contribution of a single predictor 
+# holding all the others fixed
+plot(model_gam2)
