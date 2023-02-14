60e10dfbde3186df617f2d1ed2b666cdca406ce6
lucric98 <luca1.riccardi@mail.polimi.it>
2023-02-14 22:02:22 +0100

    sistemato codice

--- 08_Conformal_prediction_model.R
+++ 08_Conformal_prediction_model.R
@@ -39,7 +39,7 @@ seed = 26111992
 data.e <- daisy(data,metric = "gower")
 data.em <- hclust(data.e, method = "mcquitty")
 clustering.m2 <- cutree(data.em,k=2)
-
+data$Van <- as.factor(clustering.m2)
 ########### TOTAL_PRICE = PRICE + CONSUMPTION\*KM_LIFE*LCOC
 
 # - PRICE <- Obtained thanks to Conformal Prediction thanks to our model
@@ -54,37 +54,36 @@ plot(Vehicles$Length,Vehicles$Height,col=Vehicles$Drive)
 # Non c'è relazione lineare ttra prezzo e consumption!!!
 plot(Vehicles$Price,Vehicles$Consumption,col=Vehicles$Drive)
 
-## se voglio predire l'altezza delle macchine devo arrivare fino all'auto più alta, tralasciando i furgoni
-# GRIGLIA PER LE AUTOMOBILI
-length.grid <- seq(min(Vehicles$Length), max(Vehicles$Length), length = 50)
-height.grid <- seq(min(Vehicles$Height), max(Vehicles$Height[Vehicles$Height<1800]), length = 50)
-
-grid <- expand.grid(LENGTH = length.grid, HEIGHT = height.grid, Drive = c("Front", "Rear","AWD"), Van="1")
-# GRIGLIA PER I VAN
-length.grid_van <- seq(min(Vehicles$Length[Vehicles$Height>1800]),
-                       max(Vehicles$Length[Vehicles$Height>1800]),
-                       length = 20)
-height.grid_van <- seq(min(Vehicles$Height[Vehicles$Height>1800]),
-                       max(Vehicles$Height[Vehicles$Height>1800]),
-                       length = 20)
-grid_van <- expand.grid(LENGTH = length.grid_van, HEIGHT = height.grid_van, Drive = c("Front"), Van="2")
-# GRIGLIA TOTALE
-total_pred_grid <- rbind(grid,grid_van)
-n <- dim(total_pred_grid)[1]
+# ## se voglio predire l'altezza delle macchine devo arrivare fino all'auto più alta, tralasciando i furgoni
+# # GRIGLIA PER LE AUTOMOBILI
+# length.grid <- seq(min(Vehicles$Length), max(Vehicles$Length), length = 50)
+# height.grid <- seq(min(Vehicles$Height), max(Vehicles$Height[Vehicles$Height<1800]), length = 50)
+# 
+# grid <- expand.grid(LENGTH = length.grid, HEIGHT = height.grid, Drive = c("Front", "Rear","AWD"), Van="1")
+# # GRIGLIA PER I VAN
+# length.grid_van <- seq(min(Vehicles$Length[Vehicles$Height>1800]),
+#                        max(Vehicles$Length[Vehicles$Height>1800]),
+#                        length = 20)
+# height.grid_van <- seq(min(Vehicles$Height[Vehicles$Height>1800]),
+#                        max(Vehicles$Height[Vehicles$Height>1800]),
+#                        length = 20)
+# grid_van <- expand.grid(LENGTH = length.grid_van, HEIGHT = height.grid_van, Drive = c("Front"), Van="2")
+# # GRIGLIA TOTALE
+# total_pred_grid <- rbind(grid,grid_van)
+# n <- dim(total_pred_grid)[1]
 
 # MODELLI GAMs per prezzo e CONSUMPTION
-data$Van <- as.factor(clustering.m2)
 
 ## predico prezzo
-PRICE_model <- gam(PRICE ~ s(LENGTH, bs="cr") + s(HEIGHT,bs="cr") + s(I(LENGTH*HEIGHT),bs="cr") + Van + Drive, data=data)
+PRICE_model <- gam(PRICE ~ s(LENGTH, bs="cr") + s(HEIGHT,bs="cr") + s(I(LENGTH*HEIGHT),bs="cr"), data=data)
 summary(PRICE_model)
 
-pred_PRICE <- predict(PRICE_model,newdata=total_pred_grid)
+# pred_PRICE <- predict(PRICE_model,newdata=total_pred_grid)
 ## predico consumption
-CONSUMPTION_model <- gam(CONSUMPTION ~ s(LENGTH, bs="cr") + s(HEIGHT,bs="cr") + s(I(LENGTH*HEIGHT),bs="cr") + Van + Drive, data=data)
+CONSUMPTION_model <- gam(CONSUMPTION ~ s(LENGTH, bs="cr") + s(HEIGHT,bs="cr") + s(I(LENGTH*HEIGHT),bs="cr"), data=data)
 summary(CONSUMPTION_model)
 
-pred_CONSUMPTION <- predict(CONSUMPTION_model,newdata=total_pred_grid)
+# pred_CONSUMPTION <- predict(CONSUMPTION_model,newdata=total_pred_grid)
 
 # TOTAL_PRICE = PRICE + CONSUMPTION (Wh/km) *KM_LIFE (km) *LCOC (€/KWh)
 
@@ -101,188 +100,71 @@ pred_CONSUMPTION <- predict(CONSUMPTION_model,newdata=total_pred_grid)
 ## queste variabili vanno settate facendo considerando diverse situazioni!
 km_life <- 12*20000
 lcoc <- 0.323/1000
+new_data <- data.frame(HEIGHT = 1500, LENGTH = 4500)
 
-new_data <- data.frame(HEIGHT = 1500, LENGTH = 4500, Drive = "Front", Van = "1")
-
-tot_price.grid <- data$PRICE + data$CONSUMPTION*km_life*lcoc
-
-## definire un punto per il quale si vuole calcolare l'intervallo di predizione
-x.obs <- 100000
-n <- length(x.obs)
+# tot_price.grid <- data$PRICE + data$CONSUMPTION*km_life*lcoc
 
 
-
-
-### APPLY CONFORMAL PREDICTION - Versione 2 - Exploit the model we have built in first phase
-# scaled_data <- data
-# scaled_data[,1:12] <- scale(data[,1:12])
-# ## MODELLO PRICE
-# gam_PRICE <- gam(PRICE ~ s(LENGTH, bs="cr") + s(HEIGHT,bs="cr") + s(I(LENGTH*HEIGHT),bs="cr") + clustering.m2 + Drive, data=scaled_data)
-# summary(gam_PRICE)
-# ## MODELLO CONSUMPTION
-# gam_CONSUMPTION <- gam(CONSUMPTION ~ s(HEIGHT, bs="cr") + s(LENGTH, bs="cr") + s(I(HEIGHT*LENGTH),bs="cr") + Drive, data=scaled_data)
-# summary(gam_CONSUMPTION)
-# 
-# # Let's build a Confromal prediction region exploiting the models: We buil a conformal prediction interval for the entire price of an electric vehicle using our models and just the infromation about lenght and HEIGHT of the vehicle
-# # PREDICT THE PRICE
-# length <- data$LENGTH
-# height <- data$HEIGHT
-# 
-# # here is how our data is distributed. We have to buil wisely the prediction grids 
-# plot(Vehicles$Length,Vehicles$Height,col=Vehicles$Drive)
-# 
-# length.grid <- seq(min(length), max(length), length = 300)
-# ## se voglio predire l'altezza delle macchine devo arrivare fino all'auto più alta, tralasciando i furgoni
-# height.grid <- seq(min(height), max(Vehicles$Height[Vehicles$Height<1800]), length = 300)
-# 
-# length.grid_van <- seq(min(Vehicles$Length[Vehicles$Height>1800]),
-#                    max(Vehicles$Length[Vehicles$Height>1800]),
-#                    length = 100)
-# 
-# height.grid_van <- seq(min(Vehicles$Height[Vehicles$Height>1800]),
-#                        max(Vehicles$Height[Vehicles$Height>1800]),
-#                        length = 100)
-# 
-# fdrive=cbind(length.grid,height.grid,rep("Front",300),rep("1",300))
-# rdrive=cbind(length.grid,height.grid,rep("Rear",300),rep("1",300))
-# AWDdrive=cbind(length.grid,height.grid,rep("AWD",300),rep("1",300))
-# Van = cbind(length.grid,height.grid,rep("Front",100),rep("2",100))
-# 
-# dati <- rbind(fdrive,rdrive,AWDdrive,Van)
-# 
-# names(dati)=c('length','height','Drive','Van')
-# 
-# pred=predict(model_gam,newdata=grid)
-# 
-# train_gam=function(x,y,out=NULL){
-#   colnames(x)=c('length','height','Drive','Van')
-#   train_data=data.frame(y,x)
-#   model_gam=gam(y ~ s(var1,bs='cr') + s(var2,bs='cr') + s(I(var1*var2),bs="cr"), + Drive + Van,data=train_data)
-# }
-# 
-# predict_gam=function(obj, new_x){
-#   new_x=data.frame(new_x)
-#   colnames(new_x)=c('length','height','Drive','Van')
-#   predict.gam(obj,new_x)
-# }
-# 
-# c_preds=conformal.pred(dati,
-#                        y = ,
-#                        c(median(x2),median(x1)),
-#                        alpha=0.05,
-#                        verbose=T,
-#                        train.fun = train_gam,
-#                        predict.fun = predict_gam,
-#                        num.grid.pts = 200)
-# c_preds
-# 
-# c_preds_split=conformal.pred.split(cbind(x2,x1),
-#                        y,
-#                        c(median(x2),median(x1)),
-#                        alpha=alpha,
-#                        verbose=T,
-#                        train.fun = train_gam,
-#                        predict.fun = predict_gam,
-#                        num.grid.pts = 200)
-# c_preds_split
-# 
-# # ## PREDICT THE CONSUMPTION
-# # x2.grid=seq(range(x2)[1],range(x2)[2],length.out = 100)
-# # x1.grid=seq(range(x1)[1],range(x1)[2],length.out = 100)
-# # grid=expand.grid(x2.grid,x1.grid)
-# # names(grid)=c('x2','x1')
-# # 
-# # pred=predict(model_gam,newdata=grid)
-# # 
-# # train_gam=function(x,y,out=NULL){
-# #   colnames(x)=c('var1','var2')
-# #   train_data=data.frame(y,x)
-# #   model_gam=gam(y ~ s(var1,bs='cr') + s(var2,bs='cr'),data=train_data)
-# # }
-# # 
-# # predict_gam=function(obj, new_x){
-# #   new_x=data.frame(new_x)
-# #   colnames(new_x)=c('var1','var2')
-# #   predict.gam(obj,new_x)
-# # }
-# ## CONFORMAL PREDICTION REGION
-# c_preds=conformal.pred(cbind(x2,x1),y,c(median(x2),median(x1)),alpha=alpha,verbose=T,train.fun = train_gam ,predict.fun = predict_gam,num.grid.pts = 200)
-# c_preds
-
-########### APPLY CONFORMAL PREDICTION - Versione 1 - use the data and the model at our disposal
-X <- data.frame(PRICE = data$PRICE, CONSUMPTION = data$CONSUMPTION)
-#X <- scale(X)
-n_b = n <- dim(X)[1]
-n_grid <- 1000
-wrapper_multi_conf=function(test_point){
-  
-  newdata=rbind(test_point,X)
-  # L^2 norm
-  #depth_surface_vec=rowSums(t(t(newdata)-colMeans(newdata))^2) 
-  # mahalanobis
-  depth_surface_vec= mahalanobis(newdata,colMeans(newdata),cov = cov(newdata))
-  
-  sum(depth_surface_vec[-1]>=depth_surface_vec[1])/(n+1)
+train_gam=function(x,y,out=NULL){
+  colnames(x)=c('LENGTH','HEIGHT')
+  train_data=data.frame(y,x)
+  model_gam=gam(y ~ s(LENGTH,bs='cr') + s(HEIGHT,bs='cr') + s(I(LENGTH*HEIGHT),bs="cr"), data=train_data)
 }
 
-test_grid_price <- seq(min(X[,1]) - 0.05*diff(range(X[,1])), 
-                   max(X[,1]) + 0.05*diff(range(X[,1])), 
-                   length = n_grid)
-test_grid_cons <- seq(min(X[,2]) - 0.05*diff(range(X[,2])), 
-                   max(X[,2]) + 0.05*diff(range(X[,2])), 
-                   length = n_grid)
-xy_surface=expand.grid(test_grid_price,test_grid_cons)
-names(xy_surface) <- c("PRICE", "CONSUMPTION")
-
-p.value=pbapply(xy_surface,1,wrapper_multi_conf)
-
-data_plot=cbind(p.value,xy_surface)
-alpha <- 0.1
-p_set=xy_surface[p.value>alpha,]
-poly_points=p_set[chull(p_set),]
-## Conformal prediction region per prezzo e consumption (non so a cosa possa servire ma potrebbe tornarci utile)
-ggplot() + 
-  geom_tile(data=data_plot, aes(PRICE, CONSUMPTION, fill= p.value)) +
-  geom_point(data=data.frame(X), aes(PRICE, CONSUMPTION)) + 
-  geom_polygon(data=poly_points,aes(PRICE,CONSUMPTION),color='red',size=1,alpha=alpha)
-
-### CALCOLIAMO IL CONFORMAL PREDICTION REGION PER IL PREZZO completo DI UNA AUMOBILE 
-### TOLGO GLI OUTLIER: LE OSSERVAZIONI FUORI DALLA REGIONE DI CONFORMAL PREDICTION 0.9
-out_price <- which(data$PRICE %in% boxplot(data$PRICE)$out)
-out_cons <- which(data$CONSUMPTION %in% boxplot(data$CONSUMPTION)$out)
-out_ind <- union(out_price, out_cons)
-
-datawo <- data[-out_ind,]
-tot_price <- datawo$PRICE + datawo$CONSUMPTION*km_life*lcoc
-n_grid <- 1000
-tot_price_grid <- seq(min(tot_price), max(tot_price),length = n_grid)
-
-pval_fun=numeric(n_grid)
-
-wrapper_knn=function(grid_point){
-  aug_y=c(grid_point,tot_price)
-  ncm=kNNdist(matrix(aug_y),100)
-  sum((ncm[-1]>=ncm[1]))/(n_b+1)
-} 
-wrapper_full_median=function(grid_point){
-  x.obs.aug=c(grid_point,tot_price)
-  mu=median(x.obs.aug)     # WHY ISN'T IT REMOVING THE ith OBSERVATION?
-  ncm=abs(mu - x.obs.aug)  
-  sum((ncm[-1]>=ncm[1]))/(n+1)  # first obs refers to grid_point
+predict_gam=function(obj, new_x){
+  new_x=data.frame(new_x)
+  colnames(new_x)=c('LENGTH','HEIGHT')
+  predict.gam(obj,new_x)
 }
-pval_fun = pbsapply(tot_price_grid,wrapper_knn)
-pval_fun = pbsapply(tot_price_grid,wrapper_full_median)
-# Plot the p-values
-plot(tot_price_grid, pval_fun, type='l', ylim=c(0,1))
-abline(h=c(0,1))
-abline(h=alpha, col='red', lty=2)
-points(tot_price, numeric(length(tot_price)), pch=3)
 
-# Compute the Prediction Interval
+dati <- data.frame(PRICE = data$PRICE, CONSUMPTION = data$CONSUMPTION, LENGTH = data$LENGTH, HEIGHT = data$HEIGHT)
+n <- dim(dati)[1]
+B <- 1000
+x0 <- c(median(dati$LENGTH), median(dati$HEIGHT))
 alpha <- 0.1
-PI.grid <- tot_price_grid[which(pval_fun >= alpha)]
-PI <- c(min(PI.grid), max(PI.grid))
-PI
-abline(v = PI, col='red')
-points(PI.grid, numeric(length(PI.grid)), pch=16, col='red')
 
+T.price.pred <- predict(PRICE_model, list = c(LENGTH = x0[1], HEIGHT = x0[2]))
+T.price.boot <- numeric(B)
+
+T.cons.pred <- predict(CONSUMPTION_model, list = c(LENGTH = x0[1], HEIGHT = x0[2]))
+T.cons.boot <- numeric(B)
+
+for(i in 1:B){
+  ind.boot <- sample(1:n, replace = TRUE)
+  
+  dati.boot <- dati[ind.boot,]
+  price_preds=conformal.pred(dati.boot[,3:4],
+                            y = dati.boot$PRICE,
+                            x0 = x0,
+                            alpha=alpha,
+                            verbose=F,
+                            train.fun = train_gam,
+                            predict.fun = predict_gam)
+  
+  T.price.boot[i] <- price_preds$pred
+  
+  cons_preds=conformal.pred(dati.boot[,3:4],
+                         y = dati.boot$CONSUMPTION,
+                         x0 = x0,
+                         alpha=alpha,
+                         verbose=F,
+                         train.fun = train_gam,
+                         predict.fun = predict_gam)
+  
+  T.cons.boot[i] <- cons_preds$pred
+}
+
+## PRICE
+right.quantile <- quantile(T.price.boot, 1 - alpha/2)
+left.quantile  <- quantile(T.price.boot, alpha/2)
+CI.RP_price <- c(T.price.pred - (right.quantile - T.price.pred), T.price.pred, T.price.pred - (left.quantile - T.price.pred))
+names(CI.RP_price)=c('lwr','lvl','upr')
+CI.RP_price
+## CONSUMPTION
+right.quantile <- quantile(T.cons.boot, 1 - alpha/2)
+left.quantile  <- quantile(T.cons.boot, alpha/2)
+CI.RP_cons <- c(T.cons.pred - (right.quantile - T.cons.pred), T.cons.pred, T.cons.pred - (left.quantile - T.cons.pred))
+names(CI.RP_cons)=c('lwr','lvl','upr')
+CI.RP_cons
+
+total_price <- CI.RP_price + CI.RP_cons*km_life*lcoc
